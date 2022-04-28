01a62e5d20d0fcde8c375abee709d9c4d5191c13
Showytam <1the1notorious@gmail.com>
2022-04-28 12:31:25 +0300

    changed css

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ const allLang = ['en','ua']
 
 select.addEventListener('change', changeURLLanguage)
 
-// перенаправить на url с указанием языка
 function changeURLLanguage() {
   let lang = select.value
   location.href = window.location.pathname + '#' + lang
