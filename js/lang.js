f393ac41269cc33e9a3606e5c9799f7be09b3de1
Showytam <1the1notorious@gmail.com>
2022-04-28 11:49:37 +0300

    first commit

---
+++ js/lang.js
@@ -0,0 +1,59 @@
+const langArr = {
+  unit: {
+    en: 'Portfolio',
+    ua: 'Портфоліо',
+  },
+  h1: {
+    en: 'VLADYSLAV KHOLOD <span>FRONT-END DEVELOPER</span>',
+    ua: 'ВЛАДИСЛАВ ХОЛОД <span>ФРОНТЕНД РОЗРОБНИК</span>',
+  },
+  home: {
+    en: 'Home',
+    ua: 'Головна',
+  },
+  about: {
+    en: 'About',
+    ua: 'Про мене',
+  },
+  portfolio: {
+    en: 'Portfolio',
+    ua: 'Портфоліо',
+  },
+  contact: {
+    en: 'Contacts',
+    ua: 'Контакти',
+  },
+  skills: {
+    en: 'Skills',
+    ua: 'Навички',
+  },
+  h2: {
+    en: 'About me',
+    ua: 'Про мене',
+  },
+
+  contact_footer: {
+    en: 'Contacts',
+    ua: 'Контакти',
+  },
+  phone: {
+    en: 'Phone',
+    ua: 'Телефон',
+  },
+  email: {
+    en: 'Email - ',
+    ua: 'Пошта - ',
+  },
+  local: {
+    en: 'Local - ',
+    ua: 'Місто - ',
+  },
+  sumy: {
+    en: 'Sumy',
+    ua: 'Суми',
+  },
+  about_me: {
+    en: 'Hi, I&#8242;m a frontend developer, looking to be part of a professional a team in which I could use my skills in coding, troubleshooting and timely assistance completion of projects.',
+    ua: ' Привіт, я фронтенд розробник, прагну стати частиною професійної команди, в якій я міг би використовувати свої навички у кодуванні, усуненні складних проблем та сприянні у своєчасному завершенні проектів.',
+  },
+}
---
+++ js/script.js
@@ -0,0 +1,83 @@
+let wrapper = document.querySelector('.wrapper')
+
+let pageSlider = new Swiper('.page', {
+  wrapperClass: 'page__wrapper',
+  slideClass: 'page__screen',
+  direction: 'vertical',
+  slidesPerView: 'auto',
+  paralax: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  watchOverflow: true,
+  speed: 800,
+  observer: true,
+  observeParents: true,
+  observeSlideChidren: true,
+
+  pagination: {
+    el: '.page__pagination',
+    type: 'bullets',
+    clickable: true,
+    bulletClass: 'page__bullet',
+    bulletActiveClass: 'page__bullet_active',
+  },
+
+  scrollbar: {
+    el: '.page__scroll',
+    dragClass: 'page__drag-scroll',
+    draggable: true,
+  },
+
+  init: false,
+
+  on: {
+    init: function () {
+      menuSlider()
+      wrapper.classList.add('_loaded')
+    },
+
+    slideChange: function () {
+      menuSliderRemove()
+      menuLinks[pageSlider.realIndex].classList.add('_active')
+    },
+  },
+})
+
+let menuLinks = document.querySelectorAll('.menu__link')
+
+function menuSlider() {
+  if (menuLinks.length > 0) {
+    menuLinks[pageSlider.realIndex].classList.add('_active')
+
+    for (let index = 0; index < menuLinks.length; index++) {
+      const menuLink = menuLinks[index]
+      menuLink.addEventListener('click', function (e) {
+        menuSliderRemove()
+        pageSlider.slideTo(index, 800)
+        menuLink.classList.add('_active')
+        e.preventDefault()
+      })
+    }
+  }
+}
+
+function menuSliderRemove() {
+  let menuLinkActive = document.querySelector('.menu__link._active')
+  if (menuLinkActive) {
+    menuLinkActive.classList.remove('_active')
+  }
+}
+
+pageSlider.init()
+
+
+
